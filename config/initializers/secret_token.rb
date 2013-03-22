a90d7e22876bc6f4df75cda9f77e2cad03456103
Daniel Milks <daniel.milks@effectiveuse.com>
2013-03-22 14:16:20 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Effuse0::Application.config.secret_token = 'cfbb65865482f57231fb3f5aea0652c2ccfed14b587025e15269c21b8d9f0e0959f1256fdb1e90b60387eeced432af37f25fc3974a62bab6b37c859808fd7170'
